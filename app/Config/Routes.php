93beb7b839e8b4e06736a0bec701ec6f141ccb3e
Pony <pony@ponycool.com>
2023-11-21 16:56:00 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,13 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use Config\CustomRoutes;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// 自定义路由策略
+$customRoutes = new CustomRoutes();
+$customRoutes->handle();
