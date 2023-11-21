a1a3f48d6b066ecdd24ddf2c05c57de5d52b2129
Pony <pony@ponycool.com>
2023-11-21 16:56:32 +0800

    Create Web.php

---
+++ app/Controllers/Web.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2023/11/21
+ * Time: 15:33
+ */
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class Web extends Base
+{
+
+}
