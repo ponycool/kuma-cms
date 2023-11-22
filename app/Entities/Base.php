57c168785a81de6c279d100c880fb79f01498f35
Pony <pony@ponycool.com>
2023-11-22 15:59:04 +0800

    Create Base.php

---
+++ app/Entities/Base.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2023/11/22
+ * Time: 15:48
+ */
+declare(strict_types=1);
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class Base extends Entity
+{
+
+}
