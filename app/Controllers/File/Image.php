5fc7419f7acf4e0d69fbd61c17f1dfadcc640c0c
Pony <pony@ponycool.com>
2023-11-27 11:12:38 +0800

    Create Image.php

---
+++ app/Controllers/File/Image.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2023/11/24
+ * Time: 11:39
+ */
+declare(strict_types=1);
+
+namespace App\Controllers\File;
+
+use App\Controllers\Base;
+use CodeIgniter\Files\File;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Image extends Base
+{
+    /**
+     * 输出图片
+     * @return ResponseInterface
+     */
+    public function index(): ResponseInterface
+    {
+        $filename = $this->request->getGet('n');
+        if (is_null($filename)) {
+            exit();
+        }
+        $path = WRITEPATH . 'uploads/';
+        $filePath = $path . $filename;
+        if (!file_exists($filePath)) {
+            exit();
+        }
+        $file = new File($filePath);
+        $binary = readfile($filePath);
+        return $this->response
+            ->setHeader('Content-Type', $file->getMimeType())
+            ->setHeader('Content-disposition', 'inline; filename="' . $file->getBasename() . '"')
+            ->setStatusCode(200)
+            ->setBody($binary);
+    }
+}
