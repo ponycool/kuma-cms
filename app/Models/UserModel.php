b17c7cdbe8772200a0d548436a58c8d2f17dfb82
Pony <pony@ponycool.com>
2024-02-18 14:39:10 +0800

    Update user base class

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: Pony
- * Date: 2024/01/04
- * Time: 01:48 上午
+ * Date: 2024/02/18
+ * Time: 06:38 上午
  */
 declare(strict_types=1);
 
@@ -16,6 +16,7 @@ class UserModel extends BaseModel
     protected $returnType = 'App\Entities\User';
     protected $useSoftDeletes = true;
     protected $allowedFields = [
+        'uuid',
         'account_id',
         'nickname',
     ];
@@ -24,9 +25,14 @@ class UserModel extends BaseModel
     protected $updatedField = 'updated_at';
     protected $deletedField = 'deleted_at';
     protected $validationRules = [
+        'uuid' => 'required|min_length[36]|max_length[36]',
     ];
     protected $validationMessages = [
-
+        'uuid' => [
+            'required' => 'uuid 列为必填项',
+            'min_length' => 'uuid 长度为36个字符',
+            'max_length' => 'uuid 长度为36个字符',
+        ],
     ];
     protected $skipValidation = false;
 
