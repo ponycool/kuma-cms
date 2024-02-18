d5028168815311eaa85cce4940089295c402c976
Pony <pony@ponycool.com>
2024-02-18 14:56:17 +0800

    Update upload image

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -11,5 +11,16 @@ namespace App\Services;
 
 class UserService extends BaseService
 {
-
+    /**
+     * 根据账户ID获取用户
+     * @param int $id
+     * @return array
+     */
+    public function getByAccountID(int $id): array
+    {
+        $cond = [
+            'account_id' => $id
+        ];
+        return $this->getFirstByCond($cond);
+    }
 }
