c97740d039a91bb5a3009fa04df6ff7ca65764af
Pony <pony@ponycool.com>
2024-03-13 17:07:54 +0800

    Update Setting.php

--- app/Enums/Setting.php
+++ app/Enums/Setting.php
@@ -15,6 +15,18 @@ enum Setting: string
     case SITE_URL = 'site_url';
     // 系统版本
     case VERSION = 'version';
+    // 服务热线
+    case SERVICE_PHONE = 'service_phone';
+    // 服务邮箱
+    case SERVICE_EMAIL = 'service_email';
+    // 版权
+    case COPYRIGHT = 'copyright';
     // ICP 备案
     case ICP = 'icp';
+    // 公安备案
+    case POLICE_RECORD = 'POLICE_RECORD';
+    // 统计脚本
+    case STATISTICAL_SCRIPT = 'statistical_script';
+    // 客服脚本
+    case SERVICE_SCRIPT = 'service_script';
 }
