213a344f5c658ef09804a00139f5b09a1191797f
Pony <pony@ponycool.com>
2024-03-16 16:30:36 +0800

    Update run logs

--- app/Controllers/Api/Log/Run.php
+++ app/Controllers/Api/Log/Run.php
@@ -13,23 +13,38 @@ use App\Controllers\Api\Base;
 use App\Enums\Code;
 use Carbon\Carbon;
 use CodeIgniter\Files\File;
+use PonyCool\File\File as FileUtil;
 use CodeIgniter\HTTP\ResponseInterface;
 use Exception;
 
 class Run extends Base
 {
+    private string $logFile;
+
+    public function __construct()
+    {
+        $date = Carbon::now()->toDateString();
+        $this->logFile = WRITEPATH . 'logs/log-' . $date . '.log';
+
+    }
+
     /**
      * 获取系统日志
      * @return void
      */
     public function index(): void
     {
-        $this->postFilter();
         try {
             $data = [
                 'code' => Code::OK,
                 'message' => '获取系统运行日志成功'
             ];
+            if (!file_exists($this->logFile)) {
+                throw new Exception('系统运行日志不存在');
+            }
+            $fileUtil = new FileUtil();
+            $content = $fileUtil::getFileLastLines($this->logFile, 50);
+            $data = array_merge($data, $content);
         } catch (Exception $e) {
             $data = [
                 'code' => Code::FAIL,
@@ -45,16 +60,13 @@ class Run extends Base
      */
     public function download(): ResponseInterface
     {
-        $this->postFilter();
         try {
-            $date = Carbon::now()->toDateString();
-            $logFile = WRITEPATH . 'logs/log-' . $date . '.log';
-            if (!file_exists($logFile)) {
+            if (!file_exists($this->logFile)) {
                 throw new Exception('系统运行日志不存在');
             }
 
-            $file = new File($logFile);
-            $binary = readfile($logFile);
+            $file = new File($this->logFile);
+            $binary = readfile($this->logFile);
             return $this->response
                 ->setHeader('Content-Type', $file->getMimeType())
                 ->setHeader('Content-disposition', 'attachment; filename="' . $file->getBasename() . '"')
