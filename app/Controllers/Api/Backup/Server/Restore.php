0539ea224cb770c338990f4aa8da22adc43ab4ec
Pony <pony@ponycool.com>
2024-03-21 17:13:15 +0800

    Create Restore.php

---
+++ app/Controllers/Api/Backup/Server/Restore.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2024/3/21
+ * Time: 17:12
+ */
+declare(strict_types=1);
+
+namespace App\Controllers\Api\Backup\Server;
+
+use App\Controllers\Api\Base;
+
+class Restore extends Base
+{
+    /**
+     * 从服务器的数据库备份文件恢复数据库
+     * @return void
+     */
+    public function index(): void
+    {
+
+    }
+}
