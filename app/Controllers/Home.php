5b38bab111c138a64d9b4eb8df6072a992cb38c9
Pony <pony@ponycool.com>
2024-04-12 11:33:29 +0800

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,15 @@ class Home extends Web
 {
     public function index(): void
     {
-        $this->setTitle('酷码内容管理系统')
+        $settings = self::getSettings();
+        $title = '首页 - ' . $settings['site_name' ?? '酷码内容管理系统'];
+        $description = $settings['site_description' ?? '酷码内容管理系统'];
+        $keywords = $settings['site_keywords' ?? '酷码内容管理系统'];
+        $this->setTitle($title)
+            ->setDescription($description)
+            ->setKeywords($keywords)
+            ->setTemplate('home')
+            ->setPage('home')
             ->render();
     }
 }
