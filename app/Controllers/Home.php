3e450d74d6f2911947ff2ea9224893c9c94c1d92
Pony <pony@ponycool.com>
2024-04-13 11:33:26 +0800

    Update theme

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,9 +8,9 @@ class Home extends Web
     public function index(): void
     {
         $settings = self::getSettings();
-        $title = '首页 - ' . $settings['site_name' ?? '酷码内容管理系统'];
-        $description = $settings['site_description' ?? '酷码内容管理系统'];
-        $keywords = $settings['site_keywords' ?? '酷码内容管理系统'];
+        $title = '首页 - ' . $settings['site_name'] ?? '酷码内容管理系统';
+        $description = $settings['site_description'] ?? '酷码内容管理系统';
+        $keywords = $settings['site_keywords'] ?? '酷码内容管理系统';
         $this->setTitle($title)
             ->setDescription($description)
             ->setKeywords($keywords)
