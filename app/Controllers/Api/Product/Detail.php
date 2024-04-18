13b7d2669993ab20a9097fcc8a49400127982083
Pony <pony@ponycool.com>
2024-04-18 09:48:01 +0800

    Create Detail.php

---
+++ app/Controllers/Api/Product/Detail.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2024/4/18
+ * Time: 上午9:31
+ */
+declare(strict_types=1);
+
+namespace App\Controllers\Api\Product;
+
+use App\Controllers\Api\Base;
+use App\Enums\Code;
+use App\Services\ProductService;
+use Exception;
+
+class Detail extends Base
+{
+    /**
+     * 获取产品详情
+     * @return void
+     */
+    public function index(): void
+    {
+        $this->postFilter();
+        $rules = [
+            'uuid' => [
+                'rules' => 'required|min_length[35]|max_length[37]',
+                'errors' => [
+                    'required' => '参数产品UUID[uuid]为必填项',
+                    'min_length' => '参数产品UUID[uuid]无效',
+                    'max_length' => '参数产品UUID[uuid]无效',
+                ]
+            ],
+        ];
+        $this->verifyJsonInputByRules($rules);
+        try {
+            $params = $this->getJsonInputParams();
+            $uuid = $params['uuid'] ?? null;
+            if ($this->validateUUID($uuid) !== true) {
+                throw new Exception('无效的产品UUID');
+            }
+            $svc = new ProductService();
+            $res = $svc->getByUUID($uuid);
+            if (is_null($res)) {
+                throw new Exception('获取产品失败');
+            }
+            $data = [
+                'code' => Code::OK,
+                'message' => '获取产品成功',
+            ];
+            $data = array_merge($data, $res);
+        } catch (Exception $e) {
+            $data = [
+                'code' => Code::FAIL,
+                'message' => $e->getMessage() ?: '获取产品失败',
+            ];
+        }
+        $this->render($data);
+    }
+}
