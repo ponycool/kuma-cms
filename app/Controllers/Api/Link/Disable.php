a133936aca328203475201a619e5e291ab53f2a0
Pony <pony@ponycool.com>
2024-04-22 11:30:43 +0800

    Create Disable.php

---
+++ app/Controllers/Api/Link/Disable.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2024/4/22
+ * Time: 上午11:28
+ */
+declare(strict_types=1);
+
+namespace App\Controllers\Api\Link;
+
+use App\Controllers\Api\Base;
+use App\Enums\Code;
+use App\Services\LinkService;
+use Exception;
+
+class Disable extends Base
+{
+    /**
+     * 禁用友情链接
+     * @return void
+     */
+    public function index(): void
+    {
+        $this->postFilter();
+        try {
+            $params = $this->getJsonInputParams();
+            $uuid = $params['uuid'] ?? null;
+            if ($this->validateUUID($uuid) !== true) {
+                throw new Exception('无效的友情链接UUID');
+            }
+            $svc = new LinkService();
+            $res = $svc->updateStatus($uuid, 0);
+            if ($res !== true) {
+                throw new Exception('禁用友情链接失败');
+            }
+            $data = [
+                'code' => Code::OK,
+                'message' => '禁用友情链接成功',
+            ];
+        } catch (Exception $e) {
+            $data = [
+                'code' => Code::FAIL,
+                'message' => $e->getMessage() ?: '禁用友情链接失败'
+            ];
+        }
+        $this->render($data);
+    }
+}
