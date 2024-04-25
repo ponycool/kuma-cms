a131da5b07ed29b288f6c5082779f8f0272c0bfd
Pony <pony@ponycool.com>
2024-04-25 10:21:02 +0800

    Update admin ui

---
+++ public/admin/assets/formatDate.c584a859.js
@@ -0,0 +1,1 @@
+function o(t){const n=t.getFullYear(),r=String(t.getMonth()+1).padStart(2,"0"),a=String(t.getDate()).padStart(2,"0");return`${n}-${r}-${a}`}export{o as f};
---
+++ public/admin/assets/index.20dedffd.js
@@ -0,0 +1,1 @@
+import{h as o}from"./index.1e29877d.js";function r(n){return o.post("/api/log/login",n)}function s(n){return o.post("/api/log/system",n)}function i(n){return o.post("/api/log/run",{line:n})}function g(){return o.post("/api/log/run/clear",{})}export{g as c,r as l,i as r,s};
