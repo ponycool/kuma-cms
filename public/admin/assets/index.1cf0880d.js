64cf50aa7d653b9581159496abba144bb023db5d
Pony <pony@ponycool.com>
2024-05-30 16:18:18 +0800

    Update admin ui

--- public/admin/assets/index.1cf0880d.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./index.739e45f0.js";function r(n){return o.post("/api/log/login",n)}function s(n){return o.post("/api/log/system",n)}function i(n){return o.post("/api/log/run",{line:n})}function g(){return o.post("/api/log/run/clear",{})}export{g as c,r as l,i as r,s};
--- public/admin/assets/index.633738d9.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./index.9be7a617.js";function r(n){return o.post("/api/log/login",n)}function s(n){return o.post("/api/log/system",n)}function i(n){return o.post("/api/log/run",{line:n})}function g(){return o.post("/api/log/run/clear",{})}export{g as c,r as l,i as r,s};
