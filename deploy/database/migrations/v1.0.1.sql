a62aa89d0f887ae1a803b6c02de3f3a6606425dc
Pony <pony@ponycool.com>
2025-08-08 11:24:23 +0800

    feat: add menu table

--- deploy/database/migrations/v1.0.1.sql
+++ deploy/database/migrations/v1.0.1.sql
@@ -10,15 +10,19 @@ CREATE TABLE m_menu (
                         icon           INTEGER,
                         type           TEXT      NOT NULL,
                         level          INTEGER   NOT NULL
-                            DEFAULT (1),
+                                                 DEFAULT (1),
                         link_url       TEXT,
                         link_type      TEXT,
                         single_page_id INTEGER,
+                        visible        INTEGER   NOT NULL
+                                                 DEFAULT (1),
+                        enabled        INTEGER   DEFAULT (0)
+                            NOT NULL,
                         sort_index     INTEGER   NOT NULL
-                            DEFAULT (0),
+                                                 DEFAULT (0),
                         created_at     TEXT      NOT NULL,
                         updated_at     TEXT,
                         deleted_at     TEXT,
                         deleted        INTEGER   NOT NULL
-                            DEFAULT (0)
+                                                 DEFAULT (0)
 );
